79da12dd21ab4b79cd37fb7b5319578f6479b908
Erik <elerickmontenegro@gmail.com>
2024-11-24 20:43:13 -0500

    cambia nombre video y background

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://restaurantesantjhoane.com/',
