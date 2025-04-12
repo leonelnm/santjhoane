00ab2c6362bfbfdf5f46a048a580e252be49565c
leonelnm <elerickmontenegro@gmail.com>
2025-04-12 12:42:17 +0200

    upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 import preact from '@astrojs/preact';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://restaurantesantjhoane.com/',
-  integrations: [tailwind(), sitemap(), preact()]
+  integrations: [sitemap(), preact()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
